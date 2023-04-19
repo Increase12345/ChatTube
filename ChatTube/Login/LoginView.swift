14db2d294731e4ffd5bb881a091107d2373f1d24
Nick <pretki123@ukr.net>
2023-04-19 17:29:37 -0600

    zz

--- ChatTube/Login/LoginView.swift
+++ ChatTube/Login/LoginView.swift
@@ -23,8 +23,10 @@ struct LoginView: View {
                         .textInputAutocapitalization(.never)
                     SecureField("password", text: $vm.password)
                 }
+                .shadow(radius: 5)
                 
                 SignInButton(vm: vm)
+                    .shadow(radius: 5)
                 
                 Text(vm.actionMessage)
             }
--- ChatTube/Login/LoginViewViewModel.swift
+++ ChatTube/Login/LoginViewViewModel.swift
@@ -8,8 +8,14 @@
 import Foundation
 import SwiftUI
 import Firebase
+import FirebaseStorage
+import FirebaseFirestore
 
 class LoginViewViewModel: ObservableObject {
+    let firebaseAuth = Auth.auth()
+    let firebaseStorage = Storage.storage()
+    let firestore = Firestore.firestore()
+    
     @Published var actionMessage = ""
     @Published var showImagePicker = false
     @Published var profileImage: UIImage?
@@ -27,7 +33,7 @@ class LoginViewViewModel: ObservableObject {
     }
     
     private func loginUser() {
-        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+        firebaseAuth.signIn(withEmail: email, password: password) { result, error in
             if let error = error {
                 self.actionMessage = "Failed to login \(error)"
                 return
@@ -38,7 +44,7 @@ class LoginViewViewModel: ObservableObject {
     }
     
     private func createNewAccount() {
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        firebaseAuth.createUser(withEmail: email, password: password) { result, error in
             if let error = error {
                 self.actionMessage = "Failed to create account \(error)"
                 return
@@ -46,7 +52,28 @@ class LoginViewViewModel: ObservableObject {
             
             self.actionMessage = "Succsessfully created user \(result?.user.uid ?? "")"
             
+            self.saveImageToStorage()
+        }
+    }
+    
+    private func saveImageToStorage() {
+        guard let uid = firebaseAuth.currentUser?.uid else { return }
+        let ref = firebaseStorage.reference(withPath: uid)
+        guard let imageData = profileImage?.jpegData(compressionQuality: 0.5) else { return }
+        ref.putData(imageData) { metadata, error in
+            if let error = error {
+                self.actionMessage = "Failed to push image to storage \(error)"
+                return
+            }
             
+            ref.downloadURL { url, error in
+                if let error = error {
+                    self.actionMessage = "Failed to retrive download url \(error)"
+                    return
+                }
+                
+                self.actionMessage = "Image saved"
+            }
         }
     }
 }
